33b20db7d7715389120975260c3c6c79447c4c12
farazbaber007 <farazbaber007@gmail.com>
2023-12-27 02:29:29 +0500

    exercise3
    // Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.

---
+++ typescript-node-projects/Exercise3/index.ts
@@ -0,0 +1,97 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+
+let personName: string = "faraz ul hussnain";
+// step 1 show in lowercase name
+
+//1st Method
+console.log(personName.toLowerCase());
+
+//2nd Method
+let lowercaseName: string = personName.toLowerCase();
+console.log(lowercaseName);
+
+
+// upercase Name
+console.log(personName.toUpperCase());
+
+let upercaseName: string = personName.toUpperCase();
+console.log(upercaseName);
+
+// title case Name
+// first convert it in array
+
+let words: string[] = personName.split(" ");
+let titlecaseName: string = "";
+for (let i = 0; i < words.length; i++){
+    titlecaseName +=words[i].charAt(0).toUpperCase()+words[i].slice(1).toLowerCase()+" ";
+}
+
+console.log(titlecaseName)
+
+
+// Method 1 using a function
+
+// function toTitleCase(input: string): string {
+//     return input.toLowerCase().replace(/(?:^|\s)\w/g, match => match.toUpperCase());
+// }
+
+// const originalString = "faraz ul hussnain";
+// const titleCaseString = toTitleCase(originalString);
+// console.log(titleCaseString);
+
+// Method 2 using array and map
+
+// function toTitleCase(input: string): string {
+//     const words = input.toLowerCase().split(' ');
+//     const titleCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     return titleCaseWords.join(' ');
+// }
+
+// const originalString = "faraz ul hussnain";
+// const titleCaseString = toTitleCase(originalString);
+// console.log(titleCaseString);
+
+
+
+// Method 3 using regular expression and replace function
+
+// function toTitleCase(input: string): string {
+//     return input.replace(/\w\S*/g, match => match.charAt(0).toUpperCase() + match.slice(1).toLowerCase());
+// }
+
+// const originalString = "faraz ul hussnain";
+// const titleCaseString = toTitleCase(originalString);
+// console.log(titleCaseString);
+
+
+// using for loop in function 
+
+
+// function toTitleCase(input: string): string {
+//     const words = input.toLowerCase().split(' ');
+//     for (let i = 0; i < words.length; i++) {
+//         // Capitalize the first letter of each word
+//         words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+//     }
+//     return words.join(' ');
+// }
+
+// const originalString = "faraz ul hussnain";
+// const titleCaseString = toTitleCase(originalString);
+// console.log(titleCaseString);
+
+
+
+// for loop
+
+// const originalString = "faraz ul hussnain";
+// const words = originalString.toLowerCase().split(' ');
+
+// for (let i = 0; i < words.length; i++) {
+//     // Capitalize the first letter of each word
+//     words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+// }
+
+// const titleCaseString = words.join(' ');
+// console.log(titleCaseString);
